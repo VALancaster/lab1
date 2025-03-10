196167e8c6c81d207cf1d0597a1c428d5cbccf02
VALancaster <valentin.kutergin@gmail.com>
2025-03-10 22:04:37 +0300

    добавил тесты (бездарю на пул риквест)

---
+++ laba1/base_test/test_tarraytable.cpp
@@ -0,0 +1,17 @@
+#include "TArrayTable.h"
+#include <gtest.h>
+
+TEST(TArrayTable, can_get_size)
+{}
+
+TEST(TArrayTable, can_get_element_by_index)
+{}
+
+TEST(TArrayTable, can_delete)
+{}
+
+TEST(TArrayTable, can_find)
+{}
+
+TEST(TArrayTable, can_insert)
+{}
