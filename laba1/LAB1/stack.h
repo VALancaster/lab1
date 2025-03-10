97a7bdafff5faa2fd93905fda0fe0a47a2a52e50
Ilya Baykov <snowdog289@gmail.com>
2025-03-10 22:39:49 +0300

    123321

--- laba1/LAB1/stack.h
+++ laba1/LAB1/stack.h
@@ -13,22 +13,58 @@ class TStack
 public:
 	TStack() : top(-1), size(1), pMem(new T[size]) {}
 
-	~TStack();
+	~TStack() {
+		delete[] pMem;
+	}
 
 	TStack(const TStack&) = delete;
 	TStack& operator=(const TStack&) = delete;
 
-	TStack(TStack&& other) noexcept;
+	TStack(TStack&& other) noexcept {
+		other.pMem = nullptr;
+		other.top = -1;
+		other.size = 0;
+	}
 
-	TStack& operator=(TStack&& other) noexcept;
+	TStack& operator=(TStack&& other) noexcept {
+		if (this != &other)
+		{
+			delete[] pMem;
+			top = other.top;
+			size = other.size;
+			pMem = other.pMem;
+			other.pMem = nullptr;
+			other.top = -1;
+			other.size = 0;
+		}
+		return *this;
+	};
 
-	size_t GetSize() const;
+	size_t GetSize() const {
+		return top + 1;
+	}
 
-	bool IsEmpty() const;
+	bool IsEmpty() const {
+		return top == -1;
+	}
 
-	T Pop();
+	T Pop() {
+		if (IsEmpty())
+			throw std::underflow_error("Stack is empty");
+		return pMem[top--];
+	}
 
-	void Push(const T& val);
+	void Push(const T& val) {
+		if (top == size - 1)
+		{
+			size *= 2;
+			T* tmp = new T[size];
+			std::copy(pMem, pMem + top + 1, tmp);
+			delete[] pMem;
+			pMem = tmp;
+		}
+		pMem[++top] = val;
+	}
 };
 
 #endif
